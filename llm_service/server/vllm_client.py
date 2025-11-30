028ba0eb524eec538242acec4c692081de21c478
Gohitha <gohitha@gmail.com>
2025-11-29 23:12:01 -0800

    default model is llama-3

--- llm_service/server/vllm_client.py
+++ llm_service/server/vllm_client.py
@@ -3,7 +3,7 @@ from vllm import LLMEngine, SamplingParams
 from vllm.engine.arg_utils import EngineArgs
 
 class VLLMClient:
-    def __init__(self, model_id="meta-llama/Llama-2-7b-hf", tensor_parallel_size=1):
+    def __init__(self, model_id="meta-llama/Llama-3.1-8B-Instruct", tensor_parallel_size=1):
         # Initialize vLLM engine
         engine_args = EngineArgs(
             model=model_id,
